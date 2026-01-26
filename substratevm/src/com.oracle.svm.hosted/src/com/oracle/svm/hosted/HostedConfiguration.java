8ea871581afb231103e9ace260e760c2863c9793
Gautham Venkatasubramanian <ahgamut@gmail.com>
2026-01-26 02:31:07 -0600

    CosmoLibC subclassing

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/HostedConfiguration.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/HostedConfiguration.java
@@ -390,7 +390,7 @@ public class HostedConfiguration {
     private static final class DefaultObjectFileFactory implements ObjectFileFactory {
         @Override
         public ObjectFile newObjectFile(int pageSize, Path tempDir, BigBang bb) {
-            if (LibCBase.targetLibCIs(CosmoLibC.class)) {
+            if (LibCBase.singleton() instanceof CosmoLibC) {
                 return new ELFObjectFile(pageSize, true);
             }
             return ObjectFile.getNativeObjectFile(pageSize, false);
